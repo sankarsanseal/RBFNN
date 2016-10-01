c6b9374fb28f257090a187bb16c32253b725fe36
Sankarsan Seal <sankarsanseal@icloud.com>
2016-10-01 19:31:14 +0530

    Coding is on.

--- RBFNN/main.cpp
+++ RBFNN/main.cpp
@@ -15,14 +15,26 @@ using namespace std;
 
 const char PATH[1024]="/Users/sankarsanseal/Documents/RBFNN/RBFNN";
 
-void kmean(double **data,int n,int k, int classes)
+double ** kmean(double **dinput, double **doutput,int n,int k,int dimensions, int classes)
 {
     int i;
-    srand((unsigned)time(NULL));
-    for(i=0;i<k;i++)
+    double **centroids=NULL;
+    centroids=(double **)malloc(sizeof(double *)*k*classes);
+    for(i=0;i<k*classes;i++)
     {
-        
+        centroids[i]=(double *)malloc(sizeof(double)*dimensions);
     }
+    
+    srand((unsigned)time(NULL));
+    if(centroids!=NULL)
+    {
+        for(i=0;i<k;i++)
+        {
+            
+        }
+    }
+    
+    return centroids;
     
 }
 
@@ -38,12 +50,16 @@ void wine()
     
     double **dinput;
     double **doutput;
+    double **centroids;
     int ylabel;
     
     fp=fopen(strcat(temp,"wine.train.txt"),"r");
     lines=0;
     if(fp!=NULL)
     {
+        cout<<"Enter the number of cluster for each label:";
+        cin>>k;
+        
         while(!feof(fp))
         {
             fscanf(fp,"%*lf %*lf %*lf %*lf %*d");
@@ -53,7 +69,8 @@ void wine()
         
         
         dinput=(double **)malloc(sizeof(double *)*lines);
-        doutput=(double **)malloc(sizeof(double )*lines);
+        doutput=(double **)malloc(sizeof(double *)*lines);
+        
         
         fseek(fp,0,SEEK_SET);
         
@@ -81,6 +98,7 @@ void wine()
             }
             
         }
+        centroids=kmean(dinput, doutput, lines, k, noOfDimension, noOfClasses);
         
     }
     else
@@ -95,7 +113,71 @@ void iris()
 {
     char temp[1024];
     strcpy(temp,PATH);
-    FILE * fp=fopen(strcat(temp,"iris.train.txt"),"r");
+    int lines=0;
+    FILE * fp;
+    int noOfDimension=4;
+    int noOfClasses=3;
+    int i,j,k;
+    
+    double **dinput;
+    double **doutput;
+    double **centroids;
+    int ylabel;
+    
+    fp=fopen(strcat(temp,"iris.train.txt"),"r");
+    lines=0;
+    if(fp!=NULL)
+    {
+        cout<<"Enter the number of cluster for each label:";
+        cin>>k;
+        
+        while(!feof(fp))
+        {
+            fscanf(fp,"%*lf %*lf %*lf %*lf %*d");
+            if(!feof(fp))
+                lines++;
+        }
+        
+        
+        dinput=(double **)malloc(sizeof(double *)*lines);
+        doutput=(double **)malloc(sizeof(double *)*lines);
+        
+        
+        fseek(fp,0,SEEK_SET);
+        
+        for(i=0;i<lines;i++)
+        {
+            if((dinput[i]=(double *)malloc(sizeof(double)*noOfDimension))
+               &&
+               (doutput[i]=(double *)malloc(sizeof(double)*noOfClasses))
+               )
+            {
+                
+                for(j=0;j<noOfDimension;j++)
+                {
+                    fscanf(fp,"%lf",&dinput[i][j]);
+                }
+                fscanf(fp,"%d", &ylabel);
+                
+                for(j=0;j<noOfClasses;j++)
+                {
+                    if(j==(ylabel-1))
+                        doutput[i][j]=1;
+                    else
+                        doutput[i][j]=0;
+                }
+            }
+            
+        }
+        centroids=kmean(dinput, doutput, lines, k, noOfDimension, noOfClasses);
+        
+    }
+    else
+    {
+        cout<<"Problem with File opening."<<endl;
+    }
+    
+
     
 }
 
